0bb8a02e463c8ef853c33abcbded522e7f7c95d1
SharmaMohit <mohit.sharma@tatahealth.com>
2018-11-23 15:00:56 +0530

    delete project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Projects from './Components/Projects'
 import AddProject from './Components/AddProject'
+import uuid from 'uuid'
 // import ReactDOM from 'react-dom';
 
 
@@ -17,18 +18,22 @@ class App extends Component {
     this.setState({
       projects: [
         {
+          id: uuid.v4(),
           title: "Credit Risk Modelling",
           category: "Machine Learning"
         },
         {
+          id: uuid.v4(),
           title: "Virtual Assistant for Windows",
           category: "Software Development"
         },
         {
+          id: uuid.v4(),
           title: "Intrusion Detection System",
           category: "Machine Learning"
         },
         {
+          id: uuid.v4(),
           title: "NSS Website",
           category: "Web Development"
         }
@@ -37,17 +42,24 @@ class App extends Component {
   }
 
   handleAddProject(project) {
-    // console.log(project);
     let newProjectList = this.state.projects;
     newProjectList.push(project);
     this.setState({projects:newProjectList});
   }
 
+  handleDeleteProject(id) {
+    let projectsList = this.state.projects;
+    let index = projectsList.find(x => x.id === id);
+    projectsList.splice(index, 1);
+    this.setState({projects:projectsList});
+  }
+
   render() {
     return (
       <div className="App">
+        <h1>Project Manager</h1>
         <AddProject addProject={this.handleAddProject.bind(this)} />
-        <Projects projects={this.state.projects} />
+        <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)} />
       </div>
     );
   }
--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 // import ReactDOM from 'react-dom';
 
 class ProjectItem extends Component {
+    deleteProject(id) {
+        this.props.onDelete(id);
+        // console.log("Item deleted!!");
+    }
     render() {
         return (
             <li className="ProjectItem">
-                <b>{this.props.project.title}</b> - {this.props.project.category}
+                <strong>{this.props.project.title}</strong> - {this.props.project.category} <a href="#" onClick={this.deleteProject.bind(this, this.props.project.id)}>X</a>
             </li>
         );
     }
